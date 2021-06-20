39e867346b4e540e7c36e255d3065addfab6d4bd
BoboFrank <anozie201040@gmail.com>
2021-06-20 05:17:30 +0100

    initial cloning and first edit

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,29 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
+
 logTitle('Title')
-/* coding examples */
+
+
+// javascript objects
+
+/* let person = {
+    name: 'Bobo',
+    age: 27,
+    hasPassport: true,
+    address: {
+        house_number: 1,
+        streetname: 'Redeemer Avenue',
+        country: 'Nigeria'
+    }
+};
+log(person);
+log(JSON.stringify(person));
+log(person.name);
+log(person.age);
+log(person.hasPassport);
+log(Object.values(person));
+log(JSON.stringify(person.address)); 
+*/
+
+// javascript arrays
